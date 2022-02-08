ca1f9c5152b89616f634637ab832615b3e04df11
Billions Joel <joelatugonza@gmail.com>
2022-02-08 04:53:06 +0300

    Add global styles and in js css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import Header from './components/Header';
+
 function App() {
-	return <h1>Hello from the app component </h1>;
+	return (
+		<>
+			<Header />
+			<div className="container">
+				<h1>My App</h1>
+			</div>
+		</>
+	);
 }
 
 export default App;
