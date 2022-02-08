34f546467b2c19328cb6fc5d1e76fa7e5d68abea
Billions Joel <joelatugonza@gmail.com>
2022-02-08 04:58:18 +0300

    Add card styles and import feedbackItem

---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,10 @@
+function FeedbackItem() {
+	return (
+		<div className="card">
+			<div className="num-display">10</div>
+			<div className="text-display">This is some text</div>
+		</div>
+	);
+}
+
+export default FeedbackItem;
