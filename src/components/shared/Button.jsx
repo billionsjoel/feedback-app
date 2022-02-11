5d8b1a0147e470775c5cabe0c68816fd164bc3bc
Billions Joel <joelatugonza@gmail.com>
2022-02-11 18:14:40 +0300

    Import prop types

--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -1,4 +1,7 @@
-function Button({ children, version, type, isDasabled }) {
+import PropTypes from 'prop-types'
+
+function Button({ children, version, type, isDasabled })
+{
 	return (
 		<button type={type} disabled={isDasabled} className={`btn btn-${version}`}>
 			{children}
