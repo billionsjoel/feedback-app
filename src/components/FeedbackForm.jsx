0166ca5a0e07ef28f3c9d2686af2958dbb0556e9
Billions Joel <joelatugonza@gmail.com>
2022-02-12 01:05:09 +0300

    Pass selected prop as a function

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -27,7 +27,7 @@ function FeedbackForm() {
 		<Card>
 			<form>
 				<h2>How would you rate your service with us ?</h2>
-				<RatingSelect />
+				<RatingSelect selected={() => {}} />
 				<div className="input-group">
 					<input
 						onChange={handleTextChange}
