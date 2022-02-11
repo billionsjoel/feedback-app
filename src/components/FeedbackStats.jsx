1af2cb1dc20953a269b8c69c8c8f100ece479691
Billions Joel <joelatugonza@gmail.com>
2022-02-11 15:08:11 +0300

    Set prop type

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 function FeedbackStats({ feedback }) {
 	//calculate avg rating
 
@@ -15,5 +17,9 @@ function FeedbackStats({ feedback }) {
 		</div>
 	);
 }
+
+FeedbackStats.propTypes = {
+	feedback: PropTypes.array.isRequired,
+};
 
 export default FeedbackStats;
