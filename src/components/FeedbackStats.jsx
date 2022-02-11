b46dde245e9c67386f162b137cd0a9fe771b889c
Billions Joel <joelatugonza@gmail.com>
2022-02-11 14:54:20 +0300

    Calc averge

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,4 +1,11 @@
 function FeedbackStats({ feedback }) {
+	//calculate avg rating
+
+	let Average =
+		feedback.reduce((acc, cur) => {
+			return acc + cur.rating;
+		}, 0) / feedback.length;
+
 	return (
 		<div className="feedback-stats">
 			<h4>{feedback.length} Reviews</h4>
