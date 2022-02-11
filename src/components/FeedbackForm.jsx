0305ba4515d712fe01f8514acba417fa9371b25e
Billions Joel <joelatugonza@gmail.com>
2022-02-11 15:30:44 +0300

    Add onchange event on input

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -9,7 +9,11 @@ function FeedbackForm() {
 				<h2>How would you rate your service with us ?</h2>
 				{/*@todo - rating select component */}
 				<div className="input-group">
-					<input type="text" placeholder="Write a review" />
+					<input
+						onChange={handleTextChange}
+						type="text"
+						placeholder="Write a review"
+					/>
 					<button type="submit">Send</button>
 				</div>
 			</form>
