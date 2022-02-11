bf34f1c7de77e62d92abc33a35f991d3e7bebd8d
Billions Joel <joelatugonza@gmail.com>
2022-02-12 01:41:12 +0300

    Add animatePresence on feedback list

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -7,11 +7,21 @@ function FeedbackList({ feedback, handleDelete }) {
 	}
 	return (
 		<div className="feedback-list">
-			{feedback.map((item) => (
-				<FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
-			))}
+			<animatePresence>
+				{feedback.map((item) => (
+					
+					<FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+				))}
+			</animatePresence>
 		</div>
 	);
+	//return (
+	//	<div className="feedback-list">
+	//		{feedback.map((item) => (
+	//			<FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+	//		))}
+	//	</div>
+	//);
 }
 
 export default FeedbackList;
