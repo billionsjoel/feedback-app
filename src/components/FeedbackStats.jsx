2bd3ec042fb4740c7f8f23a3a70501ec13473c49
Billions Joel <joelatugonza@gmail.com>
2022-02-11 14:50:02 +0300

    Refactor prop and class

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,6 +1,6 @@
-function FeedbackStats(feedback) {
+function FeedbackStats({ feedback }) {
 	return (
-		<div className="feedback-stas">
+		<div className="feedback-stats">
 			<h4>{feedback.length} Reviews</h4>
 			<h4>Average Rating: 10</h4>
 		</div>
