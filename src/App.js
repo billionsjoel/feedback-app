4571ac536031e1653df41f347b2a7f2f66fc8c0a
Billions Joel <joelatugonza@gmail.com>
2022-02-12 18:56:56 +0300

    Import link in app.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
 import feedbackData from './Data/FeedbackData';
+import AboutLinkIcon from './components/AboutLinkIcon';
 import AboutPage from './components/Pages/AboutPage';
 
 function App() {
