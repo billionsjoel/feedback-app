48f251e6a41c7b7182bb914cea51036f2112fd20
Billions Joel <joelatugonza@gmail.com>
2022-02-12 14:11:53 +0300

    Add link to go back to home

--- src/components/Pages/AboutPage.jsx
+++ src/components/Pages/AboutPage.jsx
@@ -5,7 +5,17 @@ function AboutPage() {
 		<Card>
 			<div className="about">
 				<h1>About this page </h1>
-				<p>This is a react app router test</p>
+				<p>
+					This is a react app router test. Lorem ipsum dolor sit amet
+					consectetur, adipisicing elit. Et rem voluptates eum maiores
+					recusandae doloribus corrupti blanditiis dolorum esse dignissimos,
+					iure ipsam exercitationem velit, consectetur minus a error adipisci
+					cum?
+				</p>
+				<p>Version : 1.0.0</p>
+				<p>
+					<a href="">Back to Home</a>
+				</p>
 			</div>
 		</Card>
 	);
