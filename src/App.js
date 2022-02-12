e4453e83e499deb827144b6e5bbcb0e89cb95e91
Billions Joel <joelatugonza@gmail.com>
2022-02-12 14:17:18 +0300

    Using update all components to use element for v6 routing

--- src/App.js
+++ src/App.js
@@ -26,11 +26,20 @@ function App() {
 			<Header />
 			<div className="container">
 				<Routes>
-					<Route exact path="/">
-						<FeedbackForm handleAdd={addFeedback} />
-						<FeedbackStats feedback={feedback} />
-						<FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-					</Route>
+					<Route
+						exact
+						path="/"
+						element={
+							<>
+								<FeedbackForm handleAdd={addFeedback} />
+								<FeedbackStats feedback={feedback} />
+								<FeedbackList
+									feedback={feedback}
+									handleDelete={deleteFeedback}
+								/>
+							</>
+						}
+					></Route>
 					<Route path="/about" element={<AboutPage />} />
 				</Routes>
 			</div>
