25903efe0dec15a23d6f90d7c1f4d73b7802296e
Billions Joel <joelatugonza@gmail.com>
2022-03-05 17:27:35 +0300

    Set new state using context

--- src/context/Feedbackcontext.js
+++ src/context/Feedbackcontext.js
@@ -3,5 +3,11 @@ import { createContext, useState } from 'react';
 const feedBackContext = createContext();
 
 export const feedBackProvider = (children) => {
+	const [feedback, setFeedBack] = useState([
+		{
+			id: 1,
+			text: 'This item is from context',
+		},
+	]);
 	return <feedBackContext.Provider>{children}</feedBackContext.Provider>;
 };
