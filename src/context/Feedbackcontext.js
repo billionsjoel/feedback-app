fba781e35556ff283f8507033b51d4422690d2aa
Billions Joel <joelatugonza@gmail.com>
2022-03-05 17:25:18 +0300

    Create a provider function to handle props for every component

--- src/context/Feedbackcontext.js
+++ src/context/Feedbackcontext.js
@@ -1,3 +1,7 @@
 import { createContext, useState } from 'react';
 
 const feedBackContext = createContext();
+
+export const feedBackProvider = (children) => {
+	return <feedBackContext.Provider>{children}</feedBackContext.Provider>;
+};
